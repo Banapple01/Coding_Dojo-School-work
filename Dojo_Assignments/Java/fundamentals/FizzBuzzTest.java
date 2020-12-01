61d7e933250980d62e0cb51dfa7015164e3803dd
Banapple01 <48165909+Banapple01@users.noreply.github.com>
2020-11-30 20:11:35 -0700

    Java Fundamentals
    FizzBuzz

---
+++ Dojo_Assignments/Java/fundamentals/FizzBuzzTest.java
@@ -0,0 +1,8 @@
+public class FizzBuzzTest {
+    public static void main(String[] args) {
+        int num = 16;
+        FizzBuzz response = new FizzBuzz();
+        String res = response.fizzBuzz(num);
+        System.out.println(res);
+    }
+}
