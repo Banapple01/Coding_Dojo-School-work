c1c15a416ea9035b373ce5b6154a85960eb7c67f
Banapple01 <48165909+Banapple01@users.noreply.github.com>
2020-11-30 17:59:06 -0700

    first two java assignments

---
+++ Dojo_Assignments/Java/fundamentals/FirstJavaPro.java
@@ -0,0 +1,8 @@
+public class FirstJavaPro {
+    public static void main(String[] args){
+        System.out.println("My name is Jonathan Wright");
+        System.out.println("I am 22 years old");
+        System.out.println("My hometown is Colorado Springs");
+    }
+    
+}
