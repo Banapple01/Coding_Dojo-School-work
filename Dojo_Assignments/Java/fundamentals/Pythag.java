22db70b356ef8078969682b6e0e57e966a38fea6
Banapple01 <48165909+Banapple01@users.noreply.github.com>
2020-11-30 19:20:43 -0700

    Java Fundamentals
    Pythagorean theorem

---
+++ Dojo_Assignments/Java/fundamentals/Pythag.java
@@ -0,0 +1,6 @@
+public class Pythag {
+    public double calculateHypo(int legA, int legB) {
+        double hypo = Math.hypot(legA, legB);
+        return hypo;
+    }
+}
---
+++ Dojo_Assignments/Java/fundamentals/PythagTest.java
@@ -0,0 +1,7 @@
+public class PythagTest {
+    public static void main(String[] args) {
+        Pythag num = new Pythag();
+        double result = num.calculateHypo(8, 12);
+        System.out.println(result);
+    }
+}
