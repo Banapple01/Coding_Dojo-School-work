f667251b0f201e7406966765cf5a3729c524a620
Banapple01 <48165909+Banapple01@users.noreply.github.com>
2020-12-11 23:24:04 -0700

    assignments
    MVC project

---
+++ Dojo_Assignments/Java/mcv/src/test/java/com/Jonathan/mvc/McvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Jonathan.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
